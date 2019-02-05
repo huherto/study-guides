70ba8bf38bd7e773494679c2a57e8b26f0ed5ebc
Humberto Hernandez Torres <humberto.hernandez@autozone.com>
2019-02-05 15:05:44 -0600

    Saving old work

---
+++ batchpProcessing/src/main/java/hello/PersonItemProcessor.java
@@ -0,0 +1,30 @@
+package hello;
+
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+    private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
+
+    Random rand = new Random();
+    
+    @Override
+    public Person process(final Person person) throws Exception {
+        final String firstName = person.getFirstName().toUpperCase();
+        final String lastName = person.getLastName().toUpperCase();
+
+        final Person transformedPerson = new Person(firstName, lastName);
+        int n = rand.nextInt() % 10;        
+        Thread.sleep(n * n * 10);
+
+        log.info("Converting (" + person + ") into (" + transformedPerson + ")");
+
+        return transformedPerson;
+    }
+
+}
